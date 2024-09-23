c80235b7533ffdfb7ed1edd4c4640506bdc0d1cc
Nikolay <niky.mizov@gmail.com>
2024-09-23 15:05:59 +0200

    design is there but bugged \

--- src/See.tsx
+++ src/See.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./assets/CSS/main.scss";
 
 import SingleEvent from "./SingleEvent";
 import events from "./Events.json";
